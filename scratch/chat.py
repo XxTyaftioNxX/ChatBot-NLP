50e08aa464806967cf0b3a20f39eabd4f7e11f62
Jeevitesh Man Shrestha <jeeviteshrestha24@gmail.com>
2021-12-01 02:59:25 +0545

    started app.py
    localhost TODO: add JS

--- scratch/chat.py
+++ scratch/chat.py
@@ -26,6 +26,39 @@ model.eval()
 
 bot_name = "bankBot"
 
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    
+    return "I do not understand..."
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
+'''
 # add an exit mechanism
 print('How can i help you today?')
 while True:
@@ -51,3 +84,4 @@ while True:
                 print(f'{bot_name}: {random.choice(intent["responses"])}')
     else:
         print(f"{bot_name}: Please can you refactor the question...")
+'''
